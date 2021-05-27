bf3bcf2f0d78b07a21d83d0a93b24bf0535b2c89
John Cabang <johncabz@gmail.com>
2021-05-27 10:20:43 -0400

    Update Loading component, wrap with Content component, add  Material UI loading circular progress

--- src/components/Loading/LoadingElements.js
+++ src/components/Loading/LoadingElements.js
@@ -4,6 +4,5 @@ export const StyledLoading = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #1c1c1c;
-  height: 100vh;
+  height: 100%;
 `;
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { CircularProgress } from "@material-ui/core";
 import { StyledLoading } from "./LoadingElements";
+import { Content } from "../pages/ContentElements";
 
 const Loading = () => {
   return (
-    <StyledLoading>
-      <CircularProgress />
-    </StyledLoading>
+    <Content>
+      <StyledLoading>
+        <CircularProgress />
+      </StyledLoading>
+    </Content>
   );
 };
 
